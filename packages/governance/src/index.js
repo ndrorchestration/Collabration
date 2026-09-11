336f282b754d6c573583a223140b098b46b3d007
Andrew // Ndr "Ender" Hensel <246370637+ndrorchestration@users.noreply.github.com>
2026-09-11 01:06:14 -0400

    feat: export permission inspection

--- packages/governance/src/index.js
+++ packages/governance/src/index.js
@@ -2,3 +2,4 @@ export { decideCapability, validateAgentPrincipal } from './policy.js';
 export { assertActionEvent, createActionEvent } from './action-event.js';
 export { createRateLimiter } from './rate-limit.js';
 export { loadCapabilityMatrix, policyForAgent, validateCapabilityMatrix } from './matrix.js';
+export { buildPermissionInspection } from './inspection.js';
